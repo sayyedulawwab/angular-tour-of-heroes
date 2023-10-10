78e561e9a70bd94e3011c4102c963cda807217ee
sayyedulawwab <38081373+sayyedulawwab@users.noreply.github.com>
2023-10-11 00:24:27 +0600

    Tour of Heroes Part 5: Add navigation with routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,18 +9,12 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent {
 
-  selectedHero?: Hero;
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService, private messageService: MessageService) {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
